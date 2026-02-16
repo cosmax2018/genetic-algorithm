ca1eafca3a52624da675ec4d2d231431cb7f7cf6
Massimiliano Cosmelli <massimiliano.cosmelli@accelleron-industries.com>
2026-02-16 07:31:08 +0100

    aggiunti metodi get/set

--- ga.py
+++ ga.py
@@ -20,17 +20,38 @@ def timer(function):						# prende come parametro la funzione di cui misurare il
 class Simple_GA:
 	# classe di inizializzazione o costruttore
 	def __init__(self,populationSize, tournamentSize, mutationRate, alphabet, chromosomeLength, objectiveFunction):
-		self.REPLACE_EVERY		= 100
+        # ------------------------------------------
+        self.maxIterations      = 1000
+        self.threshold          = 0
+        self.printEvery         = 20
+		self.replaceEvery		= 100
+        # ------------------------------------------
 		self.populationSize		= populationSize
 		self.tournamentSize		= tournamentSize
 		self.mutationRate		= mutationRate
 		self.alphabet 			= alphabet
 		self.chromosomeLength	= chromosomeLength
 		self.objectiveFunction	= objectiveFunction
-		
+		# ------------------------------------------
 		self.population			= Population(populationSize,tournamentSize,mutationRate,alphabet,chromosomeLength,objectiveFunction)	# una popolazione di individui
 
 	def getResults(self):				return self.population.getResults()
+    
+    def setMaxIterations(self,maxIter): self.maxIterations = maxIter
+    
+    def getMaxIterations(self):         return self.maxIterations
+    
+    def setThreshold(self,value):       self.threshold = value
+    
+    def getThreshold(self):             return self.threshold
+    
+    def setPrintEvery(self,m):          self.printEvery = m
+    
+    def getPrintEvery(self):            return self.printEvery
+    
+    def setReplaceEvery(self,n):        self.replaceEvery = n
+    
+    def getReplaceEvery(self):          return self.replaceEvery
 	
 	def savePopulation(self,file_name):	return self.population.savePopulation(file_name)	
 	
@@ -69,21 +90,21 @@ class Simple_GA:
 		self.population.mutatePopulation()
 		
 	@timer
-	def runGa(self, maxIterations, threshold=0, m=20):
+	def runGa(self):
 		# esegue l'algoritmo genetico
 		
 		print("\033[1;1HITERATION: ")
 		
 		# the iterations
-		for iterations in range(maxIterations):
+		for iterations in range(self.maxIterations):
 			
 			print("\033[1;12H" + str(iterations))
 			
 			self.stepGa()
 			
-			self.population.printPopulation(m)		 # stampa i primi m individui della popolazione
+			self.population.printPopulation(self.printEvery)		 # stampa i primi individui della popolazione
 			
-			if iterations % self.REPLACE_EVERY == 0: # ogni tot. iterazioni rimpiazza gli individui con fitness < fitness minima (Es threshold = 30) con nuovi individui creati a caso
-				if threshold != 0:
-					self.population.replaceWhorst(threshold)
+			if iterations % self.replaceEvery == 0: # ogni tot. iterazioni rimpiazza gli individui con fitness < fitness minima (Es threshold = 30) con nuovi individui creati a caso
+				if self.threshold != 0:
+					self.population.replaceWhorst(self.threshold)
 					
